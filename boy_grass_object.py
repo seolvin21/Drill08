fd90c08012c651799d81cf35d32cc1ab5a180a1c
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-10-04 20:02:21 +0900

    updated default state

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,30 @@
 from pico2d import *
+import random
 
-# Game object class here
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self): pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -11,12 +35,40 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+def reset_world():
+    global running
+    global grass
+    global team
+
+    running = True
+    grass = Grass()
+    team = [Boy() for i in range(11)]
+
+
+def update_world():
+    grass.update()
+    for boy in team:
+        boy.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    update_canvas()
+
+
 open_canvas()
 
-# initialization code
+reset_world()
 
-# game main loop code
-
-# finalization code
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
 
 close_canvas()
